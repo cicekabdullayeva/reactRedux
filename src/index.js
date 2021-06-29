61a425edf08f16303ab072c821281eb6e8fca183
Cicek Abdullayeva <cicekabdullayeva2@gmail.com>
2021-06-29 13:02:00 -0700

    redux dev tool

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import allReducers from "./reducers";
 import { Provider } from "react-redux";
 //import {increment,decrement,signin} from "./actions"
 let store = createStore(
-  allReducers);
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 
 //store.subscribe(() => console.log(store.getState()));
